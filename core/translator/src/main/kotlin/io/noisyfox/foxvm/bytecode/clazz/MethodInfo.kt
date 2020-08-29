04ced50c0d8c29c7db29c726a5d97f21e2e84d6c
noisyfox <timemanager.rick@gmail.com>
2020-08-29 23:20:25 +1000

    Add more checks against `<clinit>`

--- core/translator/src/main/kotlin/io/noisyfox/foxvm/bytecode/clazz/MethodInfo.kt
+++ core/translator/src/main/kotlin/io/noisyfox/foxvm/bytecode/clazz/MethodInfo.kt
@@ -16,7 +16,20 @@ data class MethodInfo(
 
     val isConstructor: Boolean = INIT == name
 
+    // jvms8 §2.9 Special Methods
+    // A class or interface has at most one class or interface initialization method and is
+    // initialized (§5.5) by invoking that method. The initialization method of a class or
+    // interface has the special name <clinit>, takes no arguments, and is void (§4.3.3).
     val isClassInitializer: Boolean = CLINIT == name
+        && descriptor.returnType == Type.VOID_TYPE
+        && descriptor.argumentTypes.isEmpty()
+        // In a class file whose version number is 51.0 or above, the method must
+        // additionally have its ACC_STATIC flag (§4.6) set in order to be the class or interface
+        // initialization method.
+        // This requirement was introduced in Java SE 7. In a class file whose version number is 50.0
+        // or below, a method named <clinit> that is void and takes no arguments is considered the
+        // class or interface initialization method regardless of the setting of its ACC_STATIC flag.
+        && (declaringClass.version <= 50 || isStatic)
 
     val isFinalizer: Boolean = FINALIZE == name
 
