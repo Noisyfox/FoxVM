402b999be6d5d1e1d2b8d8069d82cde66dc8aae2
noisyfox <timemanager.rick@gmail.com>
2020-09-12 03:30:05 +1000

    Fix issue of constant pool index

--- core/translator/src/main/kotlin/io/noisyfox/foxvm/translator/cgen/StringConstantPool.kt
+++ core/translator/src/main/kotlin/io/noisyfox/foxvm/translator/cgen/StringConstantPool.kt
@@ -1,15 +1,15 @@
 package io.noisyfox.foxvm.translator.cgen
 
 class StringConstantPool : Iterable<String> {
-    private val pool: LinkedHashSet<String> = LinkedHashSet()
+    private val pool: LinkedHashMap<String, Int> = LinkedHashMap()
 
     fun addConstant(constant: String): Int {
-        pool.add(constant)
-
-        return pool.size - 1
+        return pool.getOrPut(constant) {
+            pool.size
+        }
     }
 
-    override fun iterator(): Iterator<String> = pool.iterator()
+    override fun iterator(): Iterator<String> = pool.keys.iterator()
 
     val count: Int
         get() = pool.size
