b0567111670511638bb9bf609769c9587df21327
noisyfox <timemanager.rick@gmail.com>
2020-09-13 19:08:27 +1000

    Implement `Class.forName0()`

--- native/runtime/rt_core/java_lang_Class.c
+++ native/runtime/rt_core/java_lang_Class.c
@@ -8,6 +8,71 @@
 #include "vm_class.h"
 #include "vm_array.h"
 #include "vm_primitive.h"
+#include "vm_classloader.h"
+#include <string.h>
+#include <stdio.h>
+
+static C_STR java_lang_Class_getClassInternalName(VM_PARAM_CURRENT_CONTEXT, jstring name) {
+    C_CSTR utfName = vmCurrentContext->jni->GetStringUTFChars(&vmCurrentContext->jni, name, NULL);
+    if (exception_occurred(vmCurrentContext) || utfName == NULL) {
+        return NULL;
+    }
+    C_STR dupName = strdup(utfName);
+    vmCurrentContext->jni->ReleaseStringUTFChars(&vmCurrentContext->jni, name, utfName);
+    if (exception_occurred(vmCurrentContext)) {
+        free(dupName);
+        return NULL;
+    }
+
+    if (!dupName) {
+        // TODO: throw OOM
+        fprintf(stderr, "Unable to dup the class name\n");
+        abort();
+    }
+    // Replace the dot in the class name to slash
+    C_STR p = dupName;
+    while (*p != '\0') {
+        if (*p == '.') {
+            *p = '/';
+        }
+        p++;
+    }
+
+    return dupName;
+}
+
+JNIEXPORT jclass JNICALL Java_java_lang_Class_forName0(VM_PARAM_CURRENT_CONTEXT, jclass cls, jstring name, jboolean initialize, jobject loader, jclass caller) {
+    C_STR internalName = java_lang_Class_getClassInternalName(vmCurrentContext, name);
+    if (exception_occurred(vmCurrentContext)) {
+        return NULL;
+    }
+
+    native_exit_jni(vmCurrentContext);
+    jclass result = NULL;
+    do {
+        JAVA_OBJECT classloader = native_dereference(vmCurrentContext, loader);
+        if (exception_occurred(vmCurrentContext)) {
+            break;
+        }
+
+        JAVA_CLASS c = NULL;
+        if (initialize) {
+            c = classloader_get_class_by_name_init(vmCurrentContext, classloader, internalName);
+        } else {
+            c = classloader_get_class_by_name(vmCurrentContext, classloader, internalName);
+        }
+        if (exception_occurred(vmCurrentContext)) {
+            break;
+        }
+        if (c) {
+            result = native_get_local_ref(vmCurrentContext, c->classInstance);
+        }
+    } while (0);
+
+    free(internalName);
+    native_enter_jni(vmCurrentContext);
+    return result;
+}
 
 JNIEXPORT jclass JNICALL Java_java_lang_Class_getPrimitiveClass(VM_PARAM_CURRENT_CONTEXT, jclass cls, jstring name) {
     C_CSTR utfName = vmCurrentContext->jni->GetStringUTFChars(&vmCurrentContext->jni, name, NULL);
